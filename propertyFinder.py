d75c31b5cb80592201b666dffc56012a018bd880
Mattazz <matthewazada101@gmail.com>
2022-08-16 00:06:26 +0800

    Included output to csv once results are finished

--- propertyFinder.py
+++ propertyFinder.py
@@ -1,11 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas
 
 userSearch = input("Input property search: ")
 link = "https://www.lamudi.com.ph/buy/?q={}".format(userSearch)
 
 soup = BeautifulSoup(requests.get(link).content, "html.parser")
 
+listTitle = []
+listLink = []
+listPrice = []
+listDesc = []
 
 for row in soup.select(".ListingCell-row"):
     title = row.h2.get_text(strip=True)
@@ -19,3 +24,22 @@ for row in soup.select(".ListingCell-row"):
     print(price)
     print(desc)
     print("=" * 80)
+    listTitle.append(title)
+    listLink.append(link)
+    listPrice.append(price)
+    listDesc.append(desc)
+    # Store results in separate lists
+    print(listTitle)
+    print(listLink)
+    print(listPrice)
+    print(listDesc)
+
+# Create dictionary file for pandas
+output = {'Title': listTitle,
+          'Link': listLink,
+          'Price': listPrice,
+          'Desc': listDesc}
+# Create pandas DataFrame
+df = pandas.DataFrame(output)
+
+df.to_csv('output.csv')
