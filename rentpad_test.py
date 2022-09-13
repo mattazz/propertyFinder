76845a6453fe6e665aacfd2bbefd2dae11afcf6c
Mattazz <matthewazada101@gmail.com>
2022-09-13 20:30:14 +0800

    starting parsing code for rentpad

--- rentpad_test.py
+++ rentpad_test.py
@@ -7,7 +7,6 @@ userSearch = input("Input property search: ")
 link = f'https://rentpad.com.ph/q/{userSearch}'
 
 soup = BeautifulSoup(requests.get(link).content, "html.parser")
-print(soup)
 
 listTitle = []
 listLink = []
@@ -15,4 +14,8 @@ listPrice = []
 listDec = []
 
 for row in soup.select(".listing-holder"):
-    title = 
+    title = row.find("span", {"itemprop": "name"})
+    if title != None:
+        title = title.get_text()
+        listTitle.append(title)
+    print(listTitle)
